13af46af6863a85337435820141610bfddb6c7cb
qsyyke <2291308094@qq.com>
2022-07-26 16:37:06 +0800

    init

---
+++ src/main/java/com/example/demo2/LoginInfoMapper.java
@@ -0,0 +1,13 @@
+package com.example.demo2;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author qsyyke
+ * @date Created in 2022/7/26 15:48
+ */
+
+@Mapper
+public interface LoginInfoMapper {
+    int delete();
+}
---
+++ src/test/java/com/example/demo2/Demo2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.demo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo2ApplicationTests {
+    //@Autowired
+    //private LoginInfoMapper mapper;
+    @Test
+    void contextLoads() {
+        //System.out.println(mapper.delete());
+    }
+
+}
